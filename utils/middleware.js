5935b690baa42356d2d27746e2d3f28c55e917ac
Stefan Ylimäki <stefan.ylimaki@gmail.com>
2022-09-30 15:23:25 +0300

    4.20 done

--- utils/middleware.js
+++ utils/middleware.js
@@ -8,8 +8,17 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
-const unknownEndpoint = (request, response) => {
+const unknownEndpoint = (request, response, next) => {
   response.status(404).send({ error: 'unknown endpoint' })
+  next()
+}
+
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    request.token = authorization.substring(7)
+  }
+  next()
 }
 
 const errorHandler = (error, request, response, next) => {
@@ -33,5 +42,6 @@ const errorHandler = (error, request, response, next) => {
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  tokenExtractor
 }
